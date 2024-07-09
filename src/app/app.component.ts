8f52e4fbe47e90a272a31ea3c39f32661512c3a7
Pedro <pedrokansas250@gmail.com>
2024-07-09 08:37:58 -0300

    Salvando as alterações de todos os componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContainerComponent } from './componentes/container/container.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { SeparadorComponent } from './componentes/separador/separador.component';
+import { ContatosComponent } from './componentes/contatos/contatos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    ContainerComponent, 
+    CabecalhoComponent,
+    SeparadorComponent,
+    ContatosComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'project-basic';
+  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
 }
