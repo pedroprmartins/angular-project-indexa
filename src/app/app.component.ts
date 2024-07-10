cd80b259ec955ecb66c5d4713473c28fbcd978a7
Pedro <pedrokansas250@gmail.com>
2024-07-10 19:44:20 -0300

    feat: Estudo em interceptors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,17 +4,25 @@ import { ContainerComponent } from './componentes/container/container.component'
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { SeparadorComponent } from './componentes/separador/separador.component';
 import { ContatosComponent } from './componentes/contatos/contatos.component';
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet, 
-    ContainerComponent, 
+    RouterOutlet,
+    ContainerComponent,
     CabecalhoComponent,
     SeparadorComponent,
     ContatosComponent
   ],
+  // providers: [
+  //   {
+  //     provide: HTTP_INTERCEPTORS,
+  //     useClass: CriarClasseInterceptor,
+  //     multi: true,
+  //   }
+  // ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
