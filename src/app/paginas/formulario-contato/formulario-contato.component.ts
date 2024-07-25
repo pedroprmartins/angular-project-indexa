93698f5459ec907024bc03d6e483f7da47e833c3
Pedro <pedrokansas250@gmail.com>
2024-07-25 17:33:43 -0300

    feat: Gera o componente do formulario de contatos.

---
+++ src/app/paginas/formulario-contato/formulario-contato.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-contato',
+  standalone: true,
+  imports: [],
+  templateUrl: './formulario-contato.component.html',
+  styleUrl: './formulario-contato.component.css'
+})
+export class FormularioContatoComponent {
+
+}
