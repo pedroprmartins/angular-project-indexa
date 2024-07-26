67bdcde6f8477ca50c457592b2d7e0567a890b5a
Pedro <pedrokansas250@gmail.com>
2024-07-26 11:36:37 -0300

    feat: Implementa bloco de contrução que define o formulário.

--- src/app/paginas/formulario-contato/formulario-contato.component.ts
+++ src/app/paginas/formulario-contato/formulario-contato.component.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
 import {ContainerComponent} from "../../componentes/container/container.component";
+import {CommonModule} from "@angular/common";
+import {SeparadorComponent} from "../../componentes/separador/separador.component";
+import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-formulario-contato',
   standalone: true,
   imports: [
-    ContainerComponent
+    CommonModule,
+    ContainerComponent,
+    SeparadorComponent,
+    ReactiveFormsModule
   ],
   templateUrl: './formulario-contato.component.html',
   styleUrl: './formulario-contato.component.css'
 })
 export class FormularioContatoComponent {
+  contatoForm: FormGroup;
 
+  constructor() {
+    this.contatoForm = new FormGroup({
+      nome: new FormControl(''),
+      telefone: new FormControl(''),
+      email: new FormControl(''),
+      aniversario: new FormControl(''),
+      redes: new FormControl(''),
+      observacoes: new FormControl('')
+    });
+  }
 }
