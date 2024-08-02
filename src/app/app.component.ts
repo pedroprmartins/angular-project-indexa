545dd4d0e7407791f541e59edfdf11c3e848da15
Pedro <pedrokansas250@gmail.com>
2024-08-01 22:24:14 -0300

    feat: Implementa refatoração e oraganização dos componentes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,52 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ContainerComponent } from './componentes/container/container.component';
-import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
-import { SeparadorComponent } from './componentes/separador/separador.component';
-import { ContatosComponent } from './componentes/contatos/contatos.component';
-import {FormsModule} from "@angular/forms";
+import {CommonModule} from "@angular/common";
 import {FormularioContatoComponent} from "./paginas/formulario-contato/formulario-contato.component";
-
-import agenda from './agenda.json';
-
-interface Contatos {
-  id: number,
-  nome: string,
-  telefone: string
-}
+import {ListaContatosComponent} from "./paginas/lista-contatos/lista-contatos.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet,
-    ContainerComponent,
-    CabecalhoComponent,
-    SeparadorComponent,
-    ContatosComponent,
-    FormsModule,
-    FormularioContatoComponent
+    CommonModule,
+    FormularioContatoComponent,
+    ListaContatosComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
-  contatos: Contatos[] = agenda;
-  filtroPorTexto: string = '';
 
-  filtrarContatosPorTexto(): Contatos[] {
-    if(!this.filtroPorTexto) {
-      return this.contatos;
-    }
-    return this.contatos.filter(contato => {
-      return contato.nome.toLowerCase().includes(this.filtroPorTexto.toLowerCase());
-    });
-  }
-
-  filtraContantoPorLetraInicial(letra: string): Contatos[] {
-    return this.filtrarContatosPorTexto().filter(contato => {
-      return contato.nome.toLowerCase().startsWith(letra);
-    });
-  }
 }
---
+++ src/app/paginas/lista-contatos/lista-contatos.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import {CabecalhoComponent} from "../../componentes/cabecalho/cabecalho.component";
+import {ContainerComponent} from "../../componentes/container/container.component";
+import {ContatosComponent} from "../../componentes/contatos/contatos.component";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {SeparadorComponent} from "../../componentes/separador/separador.component";
+import {FormularioContatoComponent} from "../formulario-contato/formulario-contato.component";
+import agenda from '../agenda.json';
+
+interface Contatos {
+  id: number,
+  nome: string,
+  telefone: string
+}
+
+@Component({
+  selector: 'app-lista-contatos',
+  standalone: true,
+  imports: [
+    ContainerComponent,
+    CabecalhoComponent,
+    SeparadorComponent,
+    ContatosComponent,
+    FormsModule,
+    FormularioContatoComponent,
+    ReactiveFormsModule
+  ],
+  templateUrl: './lista-contatos.component.html',
+  styleUrl: './lista-contatos.component.css'
+})
+export class ListaContatosComponent {
+  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
+  contatos: Contatos[] = agenda;
+  filtroPorTexto: string = '';
+
+  filtrarContatosPorTexto(): Contatos[] {
+    if(!this.filtroPorTexto) {
+      return this.contatos;
+    }
+    return this.contatos.filter(contato => {
+      return contato.nome.toLowerCase().includes(this.filtroPorTexto.toLowerCase());
+    });
+  }
+
+  filtraContantoPorLetraInicial(letra: string): Contatos[] {
+    return this.filtrarContatosPorTexto().filter(contato => {
+      return contato.nome.toLowerCase().startsWith(letra);
+    });
+  }
+}
