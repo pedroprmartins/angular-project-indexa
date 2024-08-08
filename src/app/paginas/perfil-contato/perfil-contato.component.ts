807a77246466b0b4d32521965695de42b27db82b
Pedro <pedrokansas250@gmail.com>
2024-08-08 16:16:31 -0300

    feat: Cria componente para visualizar o perfil.

---
+++ src/app/paginas/perfil-contato/perfil-contato.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-perfil-contato',
+  standalone: true,
+  imports: [],
+  templateUrl: './perfil-contato.component.html',
+  styleUrl: './perfil-contato.component.css'
+})
+export class PerfilContatoComponent {
+
+}
